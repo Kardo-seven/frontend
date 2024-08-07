f32d7c498d8acecdf174c21eb805eee00ba83a87
Lopakadude <lopakadude@gmail.com>
2024-08-07 16:53:24 +0300

    test download

--- src/components/EditAvatar/index.tsx
+++ src/components/EditAvatar/index.tsx
@@ -1,9 +1,16 @@
 import styles from './styles.module.css';
+import avatar from '../../assets/images/profile/Avatar.png';
+import ActionButton from '../ActionButton';
 
 export default function EditAvatar() {
   return (
     <div className={styles.popupContent}>
-Аватар
+      <img className={styles.popupContent__avatar}src={avatar} alt="аватар" />
+      <div className={styles.popupContent__buttons}>
+        <ActionButton title="Загрузить с устройства" download/>
+        <ActionButton title="Сделать снимок" />
+        <ActionButton title="Удалить" secondary color="black" />
+      </div>
     </div>
   );
 }
